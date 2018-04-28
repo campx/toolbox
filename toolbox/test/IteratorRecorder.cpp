67e3809db38bacfed02d472e078fa7521bf09920
markovchainy <markovchainy@users.noreply.github.com>
2018-04-28 08:52:17 +0100

    Added more equality tests for IteratorRecorder

--- toolbox/test/IteratorRecorder.cpp
+++ toolbox/test/IteratorRecorder.cpp
@@ -8,7 +8,7 @@ TEST(Toolbox, IteratorRecorder)
     using Recorder = toolbox::IteratorRecorder<Input::const_iterator>;
     auto input = Input{1, 2, 3, 4, 5, 6, 7, 8, 9};
     auto recorder = Recorder{input.cbegin()};
-    EXPECT_EQ(input.cbegin(), recorder.get());
+    EXPECT_EQ(input.cbegin(), recorder);
     EXPECT_EQ(1, *recorder);
     EXPECT_EQ(1, *recorder);
     EXPECT_EQ(2, *(++recorder));
@@ -16,9 +16,15 @@ TEST(Toolbox, IteratorRecorder)
     EXPECT_EQ(1, *recorder);
     auto begin = Recorder(input.cbegin());
     auto end = Recorder(input.cend());
-    auto back = toolbox::back(begin, end);
+    auto last = toolbox::end(begin, end);
+    EXPECT_EQ(end, end);
+    EXPECT_EQ(input.cend(), last);
+    EXPECT_EQ(input.cend(), end);
+    last = toolbox::end(begin, end);
+    EXPECT_EQ(last, toolbox::end(begin, end));
     auto reverse = Input{};
-    std::copy(std::make_reverse_iterator(back),
-              std::make_reverse_iterator(begin), std::back_inserter(reverse));
+    auto rbegin = std::make_reverse_iterator(toolbox::end(begin, end));
+    auto rend = std::make_reverse_iterator(begin);
+    std::copy(rbegin, rend, std::back_inserter(reverse));
     EXPECT_EQ((Input{9, 8, 7, 6, 5, 4, 3, 2, 1}), reverse);
 }

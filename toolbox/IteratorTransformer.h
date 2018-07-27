858639aeef78049d3bbe6d1c1175651fd92a6e06
markovchainy <markovchainy@users.noreply.github.com>
2018-07-27 07:54:37 +0100

    Added const accessors to IteratorTransformer

--- toolbox/IteratorTransformer.h
+++ toolbox/IteratorTransformer.h
@@ -1,53 +1,69 @@
 #pragma once
+
 #include <memory>
 
 namespace toolbox
 {
 
 template <typename Transform, typename Iterator>
-class IteratorTransformer
-    : public std::iterator<std::input_iterator_tag,
-                           decltype(
-                               Transform()(typename Iterator::value_type()))>
+class IteratorTransformer : public std::iterator<std::input_iterator_tag,
+                                                 decltype(Transform()(
+                                                     typename Iterator::value_type()))>
 {
 public:
     using self_type = IteratorTransformer;
 
     explicit IteratorTransformer(Iterator it = Iterator{},
                                  Transform transform = Transform{});
+
     IteratorTransformer(const IteratorTransformer& value) = default;
+
     ~IteratorTransformer() = default;
+
     IteratorTransformer(IteratorTransformer&&) = default;
+
     IteratorTransformer& operator=(const IteratorTransformer&) = default;
+
     IteratorTransformer& operator=(IteratorTransformer&&) = default;
 
     Iterator get() const;
 
     using value_type = decltype(Transform()(typename Iterator::value_type()));
-    using reference_type = value_type&;
-    using pointer_type = value_type*;
+    using reference = value_type&;
+    using const_reference = const reference;
+    using pointer = value_type*;
+    using const_pointer = value_type const*;
 
     self_type operator++();
-    self_type operator++(int dummy);
-    reference_type operator*();
-    pointer_type operator->();
+
+    const self_type operator++(int dummy);
+
+    reference operator*();
+
+    const_reference operator*() const;
+
+    pointer operator->();
+
+    const_pointer operator->() const;
 
     bool operator==(const IteratorTransformer& rhs) const;
+
     bool operator!=(const IteratorTransformer& rhs) const;
 
 private:
     Iterator it_;
     Transform transform_;
-    value_type value_;
-    bool dirty_flag_;
+    mutable value_type value_;
+    mutable bool dirty_flag_;
 
     void evaluate();
+
     void increment();
 };
 
 template <typename Transform, typename Iterator>
-IteratorTransformer<Transform, Iterator>::IteratorTransformer(
-    Iterator it, Transform transform)
+IteratorTransformer<Transform, Iterator>::IteratorTransformer(Iterator it,
+                                                              Transform transform)
     : it_(std::move(it)), transform_(transform), dirty_flag_(true)
 {
 }
@@ -67,17 +83,17 @@ void IteratorTransformer<Transform, Iterator>::increment()
 
 template <typename Transform, typename Iterator>
 typename IteratorTransformer<Transform, Iterator>::self_type
-    IteratorTransformer<Transform, Iterator>::operator++()
+IteratorTransformer<Transform, Iterator>::operator++()
 {
     increment();
     return *this;
 }
 
 template <typename Transform, typename Iterator>
-typename IteratorTransformer<Transform, Iterator>::self_type
-    IteratorTransformer<Transform, Iterator>::operator++(int dummy)
+const typename IteratorTransformer<Transform, Iterator>::self_type
+IteratorTransformer<Transform, Iterator>::operator++(int dummy)
 {
-    dummy++;
+    (void) dummy;
     auto tmp = *this;
     increment();
     return tmp;
@@ -94,31 +110,55 @@ void IteratorTransformer<Transform, Iterator>::evaluate()
 }
 
 template <typename Transform, typename Iterator>
-typename IteratorTransformer<Transform, Iterator>::reference_type
-    IteratorTransformer<Transform, Iterator>::operator*()
+typename IteratorTransformer<Transform, Iterator>::reference
+IteratorTransformer<Transform, Iterator>::operator*()
 {
     evaluate();
     return value_;
 }
 
 template <typename Transform, typename Iterator>
-typename IteratorTransformer<Transform, Iterator>::pointer_type
-    IteratorTransformer<Transform, Iterator>::operator->()
+typename IteratorTransformer<Transform, Iterator>::const_reference
+IteratorTransformer<Transform, Iterator>::operator*() const
+{
+    if (dirty_flag_)
+    {
+        value_ = transform_(*it_);
+        dirty_flag_ = false;
+    }
+    return value_;
+}
+
+template <typename Transform, typename Iterator>
+typename IteratorTransformer<Transform, Iterator>::pointer
+IteratorTransformer<Transform, Iterator>::operator->()
 {
     evaluate();
     return &value_;
 }
 
 template <typename Transform, typename Iterator>
-bool IteratorTransformer<Transform, Iterator>::
-operator==(const IteratorTransformer<Transform, Iterator>& rhs) const
+typename IteratorTransformer<Transform, Iterator>::const_pointer
+IteratorTransformer<Transform, Iterator>::operator->() const
+{
+    if (dirty_flag_)
+    {
+        value_ = transform_(*it_);
+        dirty_flag_ = false;
+    }
+    return &value_;
+}
+
+template <typename Transform, typename Iterator>
+bool IteratorTransformer<Transform, Iterator>::operator==(
+    const IteratorTransformer<Transform, Iterator>& rhs) const
 {
     return it_ == rhs.it_;
 }
 
 template <typename Transform, typename Iterator>
-bool IteratorTransformer<Transform, Iterator>::
-operator!=(const IteratorTransformer<Transform, Iterator>& rhs) const
+bool IteratorTransformer<Transform, Iterator>::operator!=(
+    const IteratorTransformer<Transform, Iterator>& rhs) const
 {
     return it_ != rhs.it_;
 }
